1f60c0e11927b00384d8112e89eb71a182fbb450
DinhQuan <quanhero2k1@gmail.com>
2024-08-06 20:54:53 +0900

    convert obj to PL

---
+++ config/config.py
@@ -0,0 +1,104 @@
+"""
+config file
+"""
+
+# import torch
+
+# TODO
+
+
+########################################################### ROBOTIC ###################################################################
+PIXEL_SIZE = 3.45  ## unit: micrometer
+LINEAR_SPEEDS = [30, 30]
+JOINT_SPEEDS = [20, 40] ### Not over 100
+FOCAL_LENGTH = 16
+RESOLUTION_X = 2448  # DEFAULT RESOLUTION
+RESOLUTION_Y = 2048  # DEFAULT RESOLUTION
+HORIZONTAL_BASELINE = 55  # 65: best value with object is black dot
+VERTICAL_BASELINE = 450  # best value with object is black dot
+SAFE_DISTANCE = 565
+ROTATE_OY_LASER = 40
+ROTATE_OX_LASER = 0  ### Negative degrees
+ERROR_POS = 3
+
+HOME_LASER = 611
+DISTANCE_LASER2OBJECT = 135
+DISTANCE_CAMERA2OBJECT = 651
+
+TEST_TARGET = [[1878, 598], [470, 1162]]
+########################################################## AI MODEL ###################################################################
+
+
+DEVICE = "cuda"  # if torch.cuda.is_available() else "cpu"
+MODEL = {
+    "SAM": {
+        "WEIGHT": "D:\\nhan\\viko\src\sam_vit_h_4b8939.pth",  # Path of your checkpoint
+        "MODEL_TYPE": "vit_h",
+    },
+    "LightWeight_SAM": {
+        "WEIGHT": "D:\\nhan\\viko\src\weight\mobile_sam.pt",  # Path of your checkpoint
+        "MODEL_TYPE": "vit_t",
+    },
+    "SEGMENT_WELD": {
+        "API_KEY": "JtRFLNmuxFdQiNLXfFJj",  # your api key
+        "PROJECT_NAME": "weld-detection-slz4d",
+        "CONFIDENT": 50,
+        "OVERLAP": 50,
+    },
+    "HQ_SAM": {
+        "WEIGHT": "D:\\nhan\\viko\src\weight\sam_hq_vit_tiny.pth",
+        "MODEL_TYPE": "vit_tiny",
+    },
+    "YOLOV9": {
+        "WEIGHT": "weight\\best.onnx"
+        # "WEIGHT": 'D:\\nhan\\viko\src\weight\\Weld_Identification_1.pt'
+    },
+    "INSPECTION": {
+        "WEIGHT": "weight\\inspection.onnx"
+    }
+}
+
+########################################################## COORDINATE #######################################################################
+X_RATIO = 2048 / 640
+Y_RATIO = 2448 / 640
+
+
+######################################################### LABEL #############################################################################
+MODEL_WELD = {0: "0_degree", 1: "weld", 2: "90_degree", 3: "other", 4: "30_degree"}
+MODEL_INSPECTION = {0: 'air-hole', 1: 'bite-edge', 2: 'broken-arc', 3: 'crack', 4: 'hollow-bead', 5: 'overlap', 6: 'slag-inclusion', 7: 'unfused'}
+
+
+CONF_MODEL_WELD = 0.6
+PIXEL_UNION = 5 #Chấp nhận lệch 5 pixel khi xác định hai obj trùng. Sử dụng trong trường hợp mối hàn nằm trên khung obj
+
+######################################################### LASER #############################################################################
+PATH_LASER_PROGRAM = 'E:\\Quan\\AutoRoboticInspection-V1\\VIKO_UltraRobot\\src\laser\\TranferData\\bin\Release\\net8.0\\win-x64\\publish\\TranferData.exe'
+# Define the azimuth (direction) and altitude (angle) of the light source
+AZIMUTH = 315  # angle between the light source and north, in degrees
+ALTITUDE = 45  # angle above the horizon
+RESOLUTION_Y_LASER = 0.1 #mm
+FREQUENCY = 650 #Hz
+
+# EXPOSURE_TIME = 1000 #us
+# IDLE_TIME = 3900 #us
+# SLEEP = 0.1
+# CONTAINER_SIZE = 2000 #lines
+# TIME_SCAN = 200/0.1/650 
+
+
+
+
+###### optimize speed #######
+
+# # Define the azimuth (direction) and altitude (angle) of the light source
+# AZIMUTH = 315  # angle between the light source and north, in degrees
+# ALTITUDE = 45  # angle above the horizon
+
+# EXPOSURE_TIME = 1000 #us
+# IDLE_TIME = 3900 #us
+# SLEEP = 0.1
+# NO_PROFILES = 650
+# RESOLUTION_LASER_Y = 100 #(um)
+# TIME_SCAN = 200000/100/650
+# CONTAINER_SIZE = 2000 # to save
+
---
+++ test/test_pointsCloud.py
@@ -0,0 +1,112 @@
+# %%
+import trimesh
+import plotly.graph_objects as go
+import numpy as np
+import plotly.offline as pyo
+
+# %%
+mesh = trimesh.load_mesh("data/obj2k.obj")
+vertices = mesh.vertices
+
+
+# %%
+x, z, y = vertices[:, 0], vertices[:, 1], vertices[:, 2]
+print(f"length:{len(x)}, {len(y)}, {len(z)}")
+
+
+points_array = np.stack((x, y, z), axis=-1)
+centroid = np.mean(points_array, axis=0)
+print(f"Original Centroid: {centroid}")
+
+# Translate the point cloud to center around the origin
+centered_points = points_array - centroid
+
+x_centered, z_centered, y_centered = (
+    centered_points[:, 0],
+    centered_points[:, 1],
+    centered_points[:, 2],
+)
+
+
+# %%
+fig = go.Figure(
+    data=[
+        go.Scatter3d(
+            x=x_centered,
+            y=y_centered,
+            z=z_centered,
+            mode="markers",
+            marker=dict(size=2, color="blue", opacity=0.5),
+        )
+    ]
+)
+
+# Update the layout
+fig.update_layout(
+    title=f"Point Cloud", scene=dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Z")
+)
+
+# Show the plot
+fig.show()
+# pyo.iplot(fig)
+
+# %%
+points_array = np.stack((x, y, z), axis=-1)
+
+# Define the target y-coordinate value
+target_y = 1.2
+
+# Check if any y-coordinates are approximately equal to target_y
+# np.isclose is used to handle floating-point precision issues
+is_y_close_to_target = np.isclose(points_array[:, 1], target_y, atol=1)
+print(is_y_close_to_target)
+
+
+filtered_points = points_array[is_y_close_to_target]
+
+_, unique_indices = np.unique(filtered_points[:, 1], return_index=True)
+unique_filtered_points = filtered_points[unique_indices]
+# Print filtered points to verify
+print("Filtered Points:\n", filtered_points)
+
+# Prepare data for Plotly
+x_filtered, y_filtered, z_filtered = (
+    unique_filtered_points[:, 0],
+    unique_filtered_points[:, 1],
+    unique_filtered_points[:, 2],
+)
+
+
+fig = go.Figure(
+    data=[
+        go.Scatter3d(
+            x=x_filtered,
+            y=y_filtered,
+            z=z_filtered,
+            mode="markers",
+            marker=dict(size=5, color="blue", opacity=0.8),
+        )
+    ]
+)
+
+# Update layout
+fig.update_layout(
+    title="Filtered 3D Points",
+    scene=dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Z"),
+)
+
+# Show the plot
+fig.show()
+# pyo.iplot(fig)  # Use `fig.show()` if running in a script outside Jupyter
+
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+# %%
